865687f7066b33df424d6f090e69c19b741096f7
firmart <firminmartin24@gmail.com>
2018-04-24 20:02:36 +0200

    add GFD

---
+++ licence3.image/morpion/src/main/java/morpion/GFD.java
@@ -0,0 +1,189 @@
+package descripteur;
+
+import ij.process.ImageProcessor;
+
+public class GFD {
+	private ImageProcessor im;
+	private float FI[][];
+	private float FR[][];
+	// private float GFD;
+	private float GFD[][];
+	private int lg;
+	private final int t = 9;
+	private final int r = 4;
+	private int ht;
+
+	/**
+	 * Création d'un GFD
+	 * 
+	 * @param imageprocessor
+	 */
+	public GFD(ImageProcessor imageProcessor) {
+		this.im = imageProcessor;
+		lg = imageProcessor.getHeight();
+		ht = imageProcessor.getWidth();
+		this.GFD = new float[r][t];
+		this.histo_GFD();
+
+	}
+
+	/**
+	 * Obtenir le vecteur du GFD
+	 * 
+	 * @return
+	 */
+	public float[][] getGFD() {
+		return GFD;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+
+		for (int col1 = 0; col1 < GFD.length; col1++) {
+			sb.append("[ ");
+			for (int col2 = 0; col2 < GFD[col1].length; col2++) {
+				sb.append(" " + GFD[col1][col2]);
+			}
+			sb.append(" ]");
+		}
+		return sb.toString();
+
+	}
+
+	/**
+	 * cette fonction calcule la force des segments pour n niveaux de gris
+	 * 
+	 */
+	// donnes ***
+	public void histo_GFD() {
+		FI = new float[50][50];
+		FR = new float[50][50];
+
+		// Calcul du barycentre (2)
+		// System.out.println("Barycentre");
+		int bx = 0;
+		int by = 0;
+		int nb_pt = 0;
+		int i, j;
+
+		// System.out.println( "taille : " + ht + " x " + lg);
+		for (i = 0; i < ht; i++)
+			for (j = 0; j < lg; j++) {
+				// System.out.println("img " + i + " " + j + " " + im.get(i,j));
+				if (im.get(i, j) > 240) {
+					bx += j;
+					by += i;
+					nb_pt++;
+				}
+			}
+		bx /= nb_pt; // image non vide
+		by /= nb_pt;
+		// System.out.println("MaxRad");
+		// "plus rapide en creant une image tampon recentre sur le barycentre" (3)
+		// Angle_max/centroid (MaxRad); (4)
+		float MaxRad = -9999;
+		// MaxRad=0;
+		float RadTMP;
+		for (i = 0; i < ht; i++)
+			for (j = 0; j < lg; j++)
+				if ((im.get(i, j)) > 200) {
+					RadTMP = (float) Math.sqrt((j - bx) * (j - bx) + (i - by) * (i - by));
+					if (RadTMP > MaxRad)
+						MaxRad = RadTMP;
+
+				}
+		// MaxRad=sqrt(lg*ht)/2.;
+		// System.out.println("MaxRad => " + MaxRad);
+		// Polar Fourier Transform (5)
+		int rad, ang;
+		// int x, y;
+		float radius, theta;
+		// float DC;
+
+		// Initialisation
+		for (rad = 0; rad < r; rad++)
+			for (ang = 0; ang < t; ang++)
+				FR[rad][ang] = FI[rad][ang] = 0;
+		// System.out.println("x => " + lg + ", y=> " + ht);
+		// Pas dans les radians!
+		for (rad = 0; rad < r; rad++) { // radial frequency - +1 d'apres alg
+			for (ang = 0; ang < t; ang++) // angular frequency - idem
+			{
+				for (i = 0; i < ht; i++)
+					for (j = 0; j < lg; j++)
+						if (im.get(i, j) != 0) {
+							// radius=sqrt((j-bx-MaxRad)*(j-bx-MaxRad)+(i-by-MaxRad)*(i-by-MaxRad));
+							// theta = atan2f(i-by-MaxRad,j-bx-MaxRad);
+							radius = (float) Math.sqrt((j - bx) * (j - bx) + (i - by) * (i - by));
+							theta = (float) Math.atan2(i - by, j - bx);
+							if (theta < 0)
+								theta += 2 * Math.PI;
+							FR[rad][ang] += Math.cos((2 * Math.PI * rad * radius / MaxRad) + ang * theta);
+							FI[rad][ang] += Math.sin((2 * Math.PI * rad * radius / MaxRad) + ang * theta);
+						}
+			}
+		}
+		// Calcul de GFD
+		// System.out.println("GFD\n");
+		float Tval = 0;
+		for (rad = 0; rad < r; rad++)
+			for (ang = 0; ang < t; ang++) {
+				if ((rad == 0) && (ang == 0)) {
+					Tval = (float) Math.sqrt((FR[0][0] * FR[0][0] + FI[0][0] * FI[0][0]));
+					GFD[0][0] = (float) (Math.sqrt((FR[0][0] * FR[0][0] + FI[0][0] * FI[0][0]))
+							/ (Math.PI * MaxRad * MaxRad));
+					// GFD = (float) (Math.sqrt((FR[0][0] * FR[0][0] + FI[0][0] * FI[0][0]))/
+					// (Math.PI * MaxRad * MaxRad));
+				} else {
+					GFD[rad][ang] = (float) (Math.sqrt((FR[rad][ang] * FR[rad][ang] + FI[rad][ang] * FI[rad][ang]))
+							/ Tval);
+					// GFD += rad * t + ang;
+					// GFD = (float) (Math.sqrt((FR[rad][ang] * FR[rad][ang] + FI[rad][ang] *
+					// FI[rad][ang])) / Tval);
+				}
+			}
+		// System.out.println("Fin calcul");
+		// float cpt =0;
+		// for(int col1 = 0; col1 < GFD.length; col1++) {
+		// for (int col2 = 0; col2 < GFD[col1].length; col2++) {
+		// System.out.println("GFD["+col1+"]["+col2+"] = " + GFD[col1][col2]);
+		// //cpt+=GFD[col1][col2];
+		// }
+		// }
+		// System.out.println(cpt);
+		// for (i = 0; i < r * t; i++)
+		// System.out.println("%d:%f " + i + " " + (GFD + i));
+
+	}
+
+	/**
+	 * Calcule la distance euclidienne entre deux GFD ( float[][]) Comme la taille
+	 * du vecteur GFD est fixe, les deux vecteurs sont de meme taille.
+	 * 
+	 * @param gfd
+	 * @return la distance euclidienne entre les deux GFD
+	 * 
+	 */
+	public float distance(GFD gfd) {
+		float res = 0;
+		for (int i = 0; i < this.GFD.length; i++) {
+			for (int j = 0; j < this.GFD[i].length; j++) {
+				res += (Math.pow(this.GFD[i][j] - gfd.getGFD()[i][j], 2));
+			}
+		}
+		return (float) Math.sqrt(res);
+	}
+
+	public float mesureSimilarite(GFD gfd) {
+		float min = 0, max = 0;
+		for (int i = 0; i < this.GFD.length; i++) {
+			for (int j = 0; j < this.GFD[i].length; j++) {
+				min += Math.min(this.GFD[i][j], gfd.getGFD()[i][j]);
+				max += Math.max(this.GFD[i][j], gfd.getGFD()[i][j]);
+			}
+		}
+		return (min / max);
+	}
+
+}
--- licence3.image/morpion/src/main/java/morpion/Morpion.java
+++ licence3.image/morpion/src/main/java/morpion/Morpion.java
@@ -1,5 +1,13 @@
 package morpion;
 
+import java.awt.Point;
+import java.awt.Polygon;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.scijava.ItemIO;
 import org.scijava.command.Command;
 import org.scijava.command.CommandService;
@@ -7,43 +15,40 @@ import org.scijava.convert.ConvertService;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
+import descripteur.GFD;
 import ij.ImagePlus;
 import ij.plugin.Duplicator;
 import ij.process.BinaryProcessor;
 import ij.process.ByteProcessor;
 import ij.process.ImageConverter;
-import ij.process.ImageProcessor;
 import net.imagej.Dataset;
-import net.imagej.ImgPlus;
 import net.imagej.ops.OpService;
-import net.imagej.ops.Ops.Image;
-import net.imglib2.img.ImagePlusAdapter;
 import net.imglib2.type.numeric.RealType;
-import net.imglib2.type.numeric.integer.UnsignedByteType;
 import projetL3.traitement.Blob;
 import projetL3.traitement.ManyBlobs;
 
 @Plugin(type = Command.class, name = "morpion", menuPath = "Plugins>Morpion")
 public class Morpion<T extends RealType<T>> implements Command {
-
+	private List<Blob> joueur1;
+	private List<Blob> joueur2;
 	@Parameter
 	CommandService cs;
-	
+
 	@Parameter
 	OpService ops;
-	
-//	@Parameter(persist = false)
-//	ImgPlus<T> image;
 
-//	@Parameter(required = false)
-//	int threshold = 80;
+	// @Parameter(persist = false)
+	// ImgPlus<T> image;
 
-//	@Parameter(type = ItemIO.INPUT)
-//	private ImgPlus<T> img;
-	
-//	@Parameter(type = ItemIO.OUTPUT)
-//	ImgPlus<T> imageConv;
-	
+	// @Parameter(required = false)
+	// int threshold = 80;
+
+	// @Parameter(type = ItemIO.INPUT)
+	// private ImgPlus<T> img;
+
+	// @Parameter(type = ItemIO.OUTPUT)
+	// ImgPlus<T> imageConv;
+
 	@Parameter
 	ConvertService conv;
 
@@ -57,19 +62,23 @@ public class Morpion<T extends RealType<T>> implements Command {
 	public void run() {
 		ImagePlus image = convertInputToImagePlus();
 		Duplicator dupli = new Duplicator();
-		
+
 		ImagePlus image_thresholded = dupli.run(image);
 		image_thresholded.getProcessor().autoThreshold();
-		
-		
-		//ImagePlus image_skeletonized = skeletonize(dupli.run(image_thresholded));
-		
-		//ImagePlus image_convolved = convolve(dupli.run(image_skeletonized));
-		
-		//image_convolved.getProcessor().threshold(127);
-		
+
+		// ImagePlus image_skeletonized = skeletonize(dupli.run(image_thresholded));
+
+		// ImagePlus image_convolved = convolve(dupli.run(image_skeletonized));
+
+		// image_convolved.getProcessor().threshold(127);
+
 		ImagePlus image_grille = getLargestConnectedComponants(dupli.run(image_thresholded));
-		
+		System.out.println("joueur 1 : ");
+		for(Blob b : joueur1)
+			System.out.println(b.getCenterOfGravity());
+		System.out.println("joueur 2 : ");
+		for(Blob b : joueur2)
+			System.out.println(b.getCenterOfGravity());
 		output = image_grille;
 	}
 
@@ -80,61 +89,92 @@ public class Morpion<T extends RealType<T>> implements Command {
 		imp = imp.duplicate();
 		return imp;
 	}
-	
+
 	private ImagePlus skeletonize(ImagePlus imp) {
 		ByteProcessor pr = (ByteProcessor) imp.getProcessor().convertToByte(true);
 		BinaryProcessor binPr = new BinaryProcessor(pr);
 		binPr.skeletonize();
-		return imp;
-	}
-	
-	private ImagePlus convolve(ImagePlus imp) {
-		ByteProcessor pr = (ByteProcessor) imp.getProcessor().convertToByte(true);
-		int[] kernel = {1,1,1,1,1,1,1,1,1};
-		BinaryProcessor binPr = new BinaryProcessor(pr);
-		binPr.convolve3x3(kernel);
-		
-		return imp;
-	}
-	
-	private ImagePlus getLargestConnectedComponants(ImagePlus imp) {
-		ManyBlobs manyBlobs = new ManyBlobs(imp);
-		manyBlobs.findConnectedComponents();
-		System.out.println(manyBlobs.size());
-		Blob greaterBlob = manyBlobs.get(0); 
-		for (Blob blob : manyBlobs) {
-			if (greaterBlob.getPerimeter() < blob.getPerimeter())
-				greaterBlob = blob;
-		}
-		imp = Blob.generateBlobImage(greaterBlob);
-		
-		return imp;
-	}
-	
-	private ImagePlus getLines(ImagePlus imp) {
-		ByteProcessor pr = (ByteProcessor) imp.getProcessor().convertToByte(true);
-		int[] kernel = {1,0,-1,2,0,-2,1,0,-1};
-		BinaryProcessor binPr = new BinaryProcessor(pr);
-		binPr.convolve3x3(kernel);
-		
+
 		return imp;
 	}
 
-//	@Override
-//	public void run() {
-//		ImagePlus imp = convertInputToImagePlus();
-//		ops.convert().int8(image);
-//		ops.threshold().huang(image);
-//		imageConv = image.copy();
-//		
-//		//cs.run("morpion", true, "image", image, "threshold", 80);
-//		
-//		//Threshold<T> t = new Threshold<>(image);
-//		//imageConv = t.binarisation(threshold);
-//		//Binarisation b = new Binarisation(imageConv.getProcessor());
-//		//ImageProcessor ip = b.binariserImageAuto();
-//		//GFD gfd = new GFD(ip);
-//			
-//	}
+	private ImagePlus convolve(ImagePlus imp) {
+		ByteProcessor pr = (ByteProcessor) imp.getProcessor().convertToByte(true);
+		int[] kernel = { 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+		BinaryProcessor binPr = new BinaryProcessor(pr);
+		binPr.convolve3x3(kernel);
+
+		return imp;
+	}
+
+	private ImagePlus getLargestConnectedComponants(ImagePlus imp) {
+		
+		ManyBlobs manyBlobs = new ManyBlobs(imp);
+		manyBlobs.findConnectedComponents();
+		joueur1 = new ArrayList<>();
+		joueur2 = new ArrayList<>();
+
+		manyBlobs = manyBlobs.filterBlobs(20, Blob.GETPERIMETER);
+
+		System.out.println(manyBlobs.size());
+
+		Blob greaterBlob = manyBlobs.get(0);
+		manyBlobs.sort(new Comparator<Blob>() {
+			@Override
+			public int compare(Blob b1, Blob b2) {
+				return (int) (b1.getPerimeter() - b2.getPerimeter());
+
+			}
+		});
+
+		greaterBlob = manyBlobs.get(manyBlobs.size() - 1);
+		
+		manyBlobs.remove(greaterBlob);
+		Map<Blob, GFD> gfds = new HashMap<>();
+		for (Blob blob : manyBlobs) {
+			gfds.put(blob, new GFD(Blob.generateBlobImage(blob).getProcessor()));
+		}
+
+		GFD g = gfds.get(manyBlobs.get(manyBlobs.size()-1));
+		for (Blob b : manyBlobs) {
+			System.out.println("Mesure simi : " + g.mesureSimilarite(gfds.get(b)));
+			if (g.mesureSimilarite(gfds.get(b)) > 0.6) {
+				joueur1.add(b);
+			}else {
+				joueur2.add(b);
+			}
+		}
+		imp = Blob.generateBlobImage(greaterBlob);
+
+		return imp;
+	}
+
+	private ImagePlus getLines(ImagePlus imp) {
+		ByteProcessor pr = (ByteProcessor) imp.getProcessor().convertToByte(true);
+		int[] kernel = { 1, 0, -1, 2, 0, -2, 1, 0, -1 };
+		BinaryProcessor binPr = new BinaryProcessor(pr);
+		binPr.convolve3x3(kernel);
+
+		return imp;
+	}
+
+    private static getPosition n
+
+	// @Override
+	// public void run() {
+	// ImagePlus imp = convertInputToImagePlus();
+	// ops.convert().int8(image);
+	// ops.threshold().huang(image);
+	// imageConv = image.copy();
+	//
+	// //cs.run("morpion", true, "image", image, "threshold", 80);
+	//
+	// //Threshold<T> t = new Threshold<>(image);
+	// //imageConv = t.binarisation(threshold);
+	// //Binarisation b = new Binarisation(imageConv.getProcessor());
+	// //ImageProcessor ip = b.binariserImageAuto();
+	// //GFD gfd = new GFD(ip);
+	//
+	// }
 
 }
